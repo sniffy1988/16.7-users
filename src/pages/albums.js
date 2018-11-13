38b6ec2d5706a6ace2ea8e78be9cacedfcdf8c01
Sniffy1988 <Sniffy1988@gmail.com>
2018-11-13 12:28:46 +0200

    add albums

--- src/pages/albums.js
+++ src/pages/albums.js
@@ -1,4 +1,30 @@
-import React from 'react';
-export default function Albums() {
-  return <div>Albums</div>;
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+export default function Albums({ match }) {
+  const [albumsdata, updateAlbums] = useState([]);
+  function handleAlbumClick(album) {
+    console.log(album);
+  }
+  useEffect(async () => {
+    try {
+      const { data } = await axios.get(
+        `https://jsonplaceholder.typicode.com/albums?userId=${match.params.id}`
+      );
+      updateAlbums(data);
+    } catch {}
+  }, []);
+  return (
+    <ul>
+      {albumsdata.map(album => (
+        <li
+          key={album.id}
+          onClick={() => {
+            handleAlbumClick(album);
+          }}
+        >
+          {album.title}
+        </li>
+      ))}
+    </ul>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,4 +2,4 @@ export { default as Home } from './Home';
 export { default as Users } from './Users';
 export { default as User } from './User';
 export { default as Posts } from './Posts';
-export { default as Albums } from './albums';
+export { default as Albums } from './Albums';
